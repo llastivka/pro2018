e99495c8589207a6b8db584ac07bce2c9863a8e6
llastivka <lenalastivka@gmail.com>
2018-05-20 20:19:01 +0200

    PRO-6 points of intersections are detected

--- OpenCVTest/OpenCVTest/main.cpp
+++ OpenCVTest/OpenCVTest/main.cpp
@@ -154,16 +154,25 @@ bool checkCorrectness() {
 	return result;
 }
 
-int main(int argc, char** argv)
+// Finds the intersection of two lines, or returns false.
+// The lines are defined by (o1, p1) and (o2, p2).
+bool intersection(Point2f o1, Point2f p1, Point2f o2, Point2f p2,
+	Point2f &r)
 {
-	//decoding !!!
-	//Mat imageCode = imread("code.png", CV_LOAD_IMAGE_COLOR);
-	//decode(imageCode);
-	//cout << checkCorrectness();
+	Point2f x = o2 - o1;
+	Point2f d1 = p1 - o1;
+	Point2f d2 = p2 - o2;
 
-	//angle points detection !!!
-	Mat src = imread("real.png", 0);
+	float cross = d1.x*d2.y - d1.y*d2.x;
+	if (abs(cross) < /*EPS*/1e-8)
+		return false;
 
+	double t1 = (x.x * d2.y - x.y * d2.x) / cross;
+	r = o1 + d1 * t1;
+	return true;
+}
+
+void detectAnglePoints(Mat src) {
 	Mat dst, cdst;
 	Canny(src, dst, 50, 200, 3);
 	cvtColor(dst, cdst, CV_GRAY2BGR);
@@ -171,7 +180,10 @@ int main(int argc, char** argv)
 	vector<Vec2f> lines;
 	// detect lines
 	HoughLines(dst, lines, 1, CV_PI / 180, 150, 0, 90);
-
+	int linesSize = lines.size();
+	cout << linesSize << endl;
+	vector<Point> linePoints1;
+	vector<Point> linePoints2;
 	// draw lines
 	for (size_t i = 0; i < lines.size(); i++)
 	{
@@ -183,13 +195,55 @@ int main(int argc, char** argv)
 		pt1.y = cvRound(y0 + 1000 * (a));
 		pt2.x = cvRound(x0 - 1000 * (-b));
 		pt2.y = cvRound(y0 - 1000 * (a));
+		linePoints1.push_back(pt1);
+		linePoints2.push_back(pt2);
 		line(cdst, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
 	}
 
+	vector<Point2f> intersectionPoints;
+	for (int i = 0; i < linePoints1.size(); i++)
+	{
+		for (int j = i + 1; j < linePoints2.size(); j++)
+		{
+			Point2f intersectionPoint;
+			intersection(linePoints1[i], linePoints2[i], linePoints1[j], linePoints2[j], intersectionPoint);
+			int thickness = -1;
+			int lineType = 8;
+			circle(cdst, intersectionPoint, 2.0, Scalar(0, 255, 0), thickness, lineType);
+			intersectionPoints.push_back(intersectionPoint);
+		}
+	}
+
+	vector<float> distances;
+	float distanceSum = 0;
+	for (int i = 0; i < intersectionPoints.size(); i++)
+	{
+		for (int j = i + 1; j < intersectionPoints.size(); j++)
+		{
+			Point diff = intersectionPoints[i] - intersectionPoints[j];
+			distanceSum += sqrt(diff.x*diff.x + diff.y*diff.y);
+		}
+	}
+	float averageDistance = distanceSum / intersectionPoints.size();
+	cout << distanceSum << endl;
+	cout << averageDistance << endl;
+	
 	imshow("source", src);
 	imshow("detected lines", cdst);
 
 	imwrite("lines.png", cdst);
+}
+
+int main(int argc, char** argv)
+{
+	//decoding !!!
+	//Mat imageCode = imread("code.png", CV_LOAD_IMAGE_COLOR);
+	//decode(imageCode);
+	//cout << checkCorrectness();
+
+	//angle points detection !!!
+	Mat src = imread("real.png", 0);
+	detectAnglePoints(src);
 
 	waitKey();
 	return 0;
