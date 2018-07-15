5c8e65d8c05f68a468380075bc9f4b03643d1213
llastivka <lenalastivka@gmail.com>
2018-07-15 16:21:59 +0200

    PRO-9 thresholding to find the brightest regions

--- OpenCVTest/OpenCVTest/MainProgram.cpp
+++ OpenCVTest/OpenCVTest/MainProgram.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 using namespace std;
 
 Mat src;
-Mat linesImg;
+Mat srcGray;
 Point2f inputQuad[4];
 Point2f outputQuad[4];
 
@@ -32,8 +32,7 @@ vector<Vec2f> calculateHoughLines(Mat src)
 	return result;
 }
 
-Mat drawLines(vector<Vec2f> lines) {
-	Mat linesImg = Mat::zeros(src.rows, src.cols, CV_LOAD_IMAGE_GRAYSCALE);
+void drawLines(Mat img, vector<Vec2f> lines) {
 	for (size_t i = 0; i < lines.size(); i++)
 	{
 		float rho = lines[i][0], theta = lines[i][1];
@@ -44,191 +43,56 @@ Mat drawLines(vector<Vec2f> lines) {
 		pt1.y = cvRound(y0 + 1000 * (a));
 		pt2.x = cvRound(x0 - 1000 * (-b));
 		pt2.y = cvRound(y0 - 1000 * (a));
-		line(linesImg, pt1, pt2, Scalar(255, 255, 255), 1, CV_AA);
+		line(img, pt1, pt2, Scalar(0, 0, 0), 3, CV_AA);
 	}
-	return linesImg;
 }
 
-//TODO will have to think about it more bacause if the image size is smaller it probably will affect this function.
-//Probably will have to make max rhoDiff and max thetaDiff dependent on the size of initial picture
-vector<Vec2f> unifyCloseLines(vector<Vec2f> lines)
+vector<Vec2i> regionForPixel(Mat img, int row, int column)
 {
-	vector<Vec2f> modified = {};
-	for (int i = 0; i < lines.size(); i++)
+	vector<Vec2i> result = {};
+	return result;
+}
+
+vector<vector<Vec2i>> detectLightRegions(Mat img) {
+	vector<vector<Vec2i>> result = {};
+	Vec3b threshold = Vec3b(25, 25, 25);
+	for (int r = 0; r < src.rows; r++)
 	{
-		if (!(lines[i][0] == 0 && lines[i][1] == 0))
+		for (int c = 0; c < src.cols; c++)
 		{
-			vector<int> closeList = {};
-			closeList.push_back(i);
-			float rho1 = lines[i][0], theta1 = lines[i][1];
-			for (int j = i + 1; j < lines.size(); j++)
+			if (img.at<Vec3b>(r, c)[0] < threshold[0] && img.at<Vec3b>(r, c)[1] < threshold[1] && img.at<Vec3b>(r, c)[2] < threshold[2])
 			{
-				if (!(lines[j][0] == 0 && lines[j][1] == 0))
-				{
-					float rho2 = lines[j][0], theta2 = lines[j][1];
-					float rhoDiff = abs(rho1 - rho2);
-					float thetaDiff = abs(theta1 - theta2);
-					if (rhoDiff < 20 && thetaDiff < 2 * CV_PI * 0.2) //rhoDiff and thetaDiff here
-					{
-						closeList.push_back(j);
-					}
-				}
+				result.push_back(regionForPixel(img, r, c));
 			}
-
-			//for testing purposes (to see close lines visually)
-			//vector<Vec2f> expLines = {};
-			//for (int k = 0; k < closeList.size(); k++)
-			//{
-			//	expLines.push_back(lines[closeList.at(k)]);
-			//}
-			//Mat exp = drawLines(expLines);
-			//imshow("mid", exp);
-			//waitKey(0);
-
-			float rhoSum = 0;
-			float thetaSum = 0;
-			for (int j = 0; j < closeList.size(); j++)
-			{
-				rhoSum += lines[closeList.at(j)][0];
-				thetaSum += lines[closeList.at(j)][1];
-				lines[j][0] = 0;
-				lines[j][1] = 0;
-			}
-			Vec2f* averageLine = new Vec2f(rhoSum / closeList.size(), thetaSum / closeList.size());
-			modified.push_back(*averageLine);
 		}
 	}
-	return modified;
-}
-
-float calculateSumOfCloseness(vector<Vec2f> lines)
-{
-	float sum = 0;
-	for (int i = 0; i < lines.size(); i++) {
-		float theta = lines[i][1];
-		while (theta > 2 * CV_PI)
-		{
-			theta = theta - 2 * CV_PI;
-		}
-		while (theta < 0)
-		{
-			theta += 2 * CV_PI;
-		}
-		if (theta >= CV_PI / 4 && theta < 3 * CV_PI / 4)
-		{
-			sum += abs(theta - CV_PI / 2);
-		}
-		else if (theta >= 3 * CV_PI / 4 && theta < 5 * CV_PI / 4)
-		{
-			sum += abs(theta - CV_PI);
-		}
-		else if (theta >= 5 * CV_PI / 4 && theta < 7 * CV_PI / 4)
-		{
-			sum += abs(theta - 3 * CV_PI / 2);
-		}
-		else
-		{
-			sum += abs(theta);
-		}
-	}
-	return sum;
-}
-
-void transform(vector<Vec2f> lines)
-{
-	float initialPointsCoordinates[4][2] = { { 0, 0 },{ src.cols - 1, 0 },{ src.cols - 1, src.rows - 1 },{ 0, src.rows - 1 } };
-	inputQuad[0] = Point2f(initialPointsCoordinates[0][0], initialPointsCoordinates[0][1]);
-	inputQuad[1] = Point2f(initialPointsCoordinates[1][0], initialPointsCoordinates[1][1]);
-	inputQuad[2] = Point2f(initialPointsCoordinates[2][0], initialPointsCoordinates[2][1]);
-	inputQuad[3] = Point2f(initialPointsCoordinates[3][0], initialPointsCoordinates[3][1]);
-
-
-	Mat currentMinTransformed = linesImg;
-	vector<Vec2f> currentMinLines = lines;
-	float currentMinSumOfCloseness = calculateSumOfCloseness(lines);
-	int shift = 1;
-	int counterFor8DifferentTypesOfChange = 0;
-
-	while (counterFor8DifferentTypesOfChange >= 0 && counterFor8DifferentTypesOfChange < 12 && calculateSumOfCloseness(lines) > 0) {
-		outputQuad[0] = inputQuad[0];
-		outputQuad[1] = inputQuad[1];
-		outputQuad[2] = inputQuad[2];
-		outputQuad[3] = inputQuad[3];
-
-		switch (counterFor8DifferentTypesOfChange) {
-		case 0: outputQuad[0] = Point2f(inputQuad[0].x + shift, inputQuad[0].y);
-			break;
-		case 1: outputQuad[0] = Point2f(inputQuad[0].x, inputQuad[0].y + shift);
-			break;
-		case 2: outputQuad[1] = Point2f(inputQuad[1].x - shift, inputQuad[1].y);
-			break;
-		case 3: outputQuad[1] = Point2f(inputQuad[1].x, inputQuad[1].y + shift);
-			break;
-		case 4: outputQuad[2] = Point2f(inputQuad[2].x - shift, inputQuad[2].y);
-			break;
-		case 5: outputQuad[2] = Point2f(inputQuad[2].x, inputQuad[2].y - shift);
-			break;
-		case 6: outputQuad[3] = Point2f(inputQuad[3].x + shift, inputQuad[3].y);
-			break;
-		case 7: outputQuad[3] = Point2f(inputQuad[3].x, inputQuad[3].y - shift);
-			break;
-		case 8: outputQuad[0] = Point2f(inputQuad[0].x + shift, inputQuad[0].y);
-			outputQuad[1] = Point2f(inputQuad[1].x - shift, inputQuad[1].y);
-			break;
-		case 9: outputQuad[1] = Point2f(inputQuad[1].x, inputQuad[1].y + shift);
-			outputQuad[2] = Point2f(inputQuad[2].x, inputQuad[2].y - shift);
-			break;
-		case 10: outputQuad[2] = Point2f(inputQuad[2].x - shift, inputQuad[2].y);
-			outputQuad[3] = Point2f(inputQuad[3].x + shift, inputQuad[3].y);
-			break;
-		case 11: outputQuad[0] = Point2f(inputQuad[0].x, inputQuad[0].y + shift);
-			outputQuad[3] = Point2f(inputQuad[3].x, inputQuad[3].y - shift);
-			break;
-		default:
-			break;
-		}
-
-		cout << "Current MIN sum of angle closeness: " << currentMinSumOfCloseness << endl;
-		Mat transformed = perspectiveTransform(linesImg);
-		vector<Vec2f> newLines = calculateHoughLines(transformed);
-		float newSumOfCloseness = calculateSumOfCloseness(newLines);
-		cout << "New current sum of angle closeness: " << newSumOfCloseness << endl;
-		//for testing purposes
-		imshow("mid", transformed);
-		waitKey();
-
-		shift++;
-		if (newSumOfCloseness - currentMinSumOfCloseness < 0.5) {
-			if (newSumOfCloseness < currentMinSumOfCloseness) {
-				currentMinSumOfCloseness = newSumOfCloseness;
-				currentMinTransformed = transformed;
-			}
-			//for testing purposes
-			imwrite("transformed.png", transformed);
-		}
-		else
-		{
-			counterFor8DifferentTypesOfChange++;
-			cout << "!!! New type of change: " << counterFor8DifferentTypesOfChange << endl;
-			shift = 1;
-			linesImg = currentMinTransformed;
-		}
-	}
+	return result;
 }
 
 int main(int argc, char** argv)
 {
-	src = imread("real.png", 1);
-	imshow("source", src);
-	vector<Vec2f> lines = calculateHoughLines(src);
-	linesImg = drawLines(lines);
-	imwrite("lines.png", linesImg);
-	vector<Vec2f> lessLines = unifyCloseLines(lines);
-	Mat linesImg1 = drawLines(lessLines);
-	imwrite("lines1.png", linesImg1);
-	transform(lessLines);
+	src = imread("white.png", CV_LOAD_IMAGE_UNCHANGED);
+	srcGray = imread("white.png", CV_LOAD_IMAGE_GRAYSCALE);
+	vector<Vec2f> lines = calculateHoughLines(srcGray);
+	medianBlur(srcGray, srcGray, 5);
+	//drawLines(srcGray, lines);
+	imshow("with lines", srcGray);
+	imwrite("lines.png", srcGray);
 
-	imshow("output", linesImg);
+	double thres = 220;
+	double color = 255;
+	threshold(srcGray, srcGray, thres, color, CV_THRESH_BINARY);
 
+	// Execute erosion to improve the detection
+	int erosion_size = 4;
+	Mat element = getStructuringElement(MORPH_CROSS,
+		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+		Point(erosion_size, erosion_size));
+	erode(srcGray, srcGray, element);
+	imshow("output", srcGray);
+
+	//vector<vector<Vec2i>> lightRegions = detectLightRegions(srcGray);
+
+	//imshow("output", src);
 	waitKey();
 }
