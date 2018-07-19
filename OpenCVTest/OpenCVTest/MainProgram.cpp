037b8b7a6aa4e5ffe0b8aac5befb978afa7cdab8
llastivka <lenalastivka@gmail.com>
2018-07-19 23:45:29 +0200

    PRO-9 pre processing for transformation with 4 white anglemodules

--- OpenCVTest/OpenCVTest/MainProgram.cpp
+++ OpenCVTest/OpenCVTest/MainProgram.cpp
@@ -47,52 +47,157 @@ void drawLines(Mat img, vector<Vec2f> lines) {
 	}
 }
 
-vector<Vec2i> regionForPixel(Mat img, int row, int column)
+Mat threasholdImage(Mat img)
 {
-	vector<Vec2i> result = {};
-	return result;
-}
+	medianBlur(img, img, 5);
 
-vector<vector<Vec2i>> detectLightRegions(Mat img) {
-	vector<vector<Vec2i>> result = {};
-	Vec3b threshold = Vec3b(25, 25, 25);
-	for (int r = 0; r < src.rows; r++)
-	{
-		for (int c = 0; c < src.cols; c++)
-		{
-			if (img.at<Vec3b>(r, c)[0] < threshold[0] && img.at<Vec3b>(r, c)[1] < threshold[1] && img.at<Vec3b>(r, c)[2] < threshold[2])
-			{
-				result.push_back(regionForPixel(img, r, c));
-			}
-		}
-	}
-	return result;
-}
-
-int main(int argc, char** argv)
-{
-	src = imread("white.png", CV_LOAD_IMAGE_UNCHANGED);
-	srcGray = imread("white.png", CV_LOAD_IMAGE_GRAYSCALE);
-	vector<Vec2f> lines = calculateHoughLines(srcGray);
-	medianBlur(srcGray, srcGray, 5);
-	//drawLines(srcGray, lines);
-	imshow("with lines", srcGray);
-	imwrite("lines.png", srcGray);
-
-	double thres = 220;
+	double thres = 240;
 	double color = 255;
-	threshold(srcGray, srcGray, thres, color, CV_THRESH_BINARY);
+	threshold(img, img, thres, color, CV_THRESH_BINARY);
 
 	// Execute erosion to improve the detection
 	int erosion_size = 4;
 	Mat element = getStructuringElement(MORPH_CROSS,
 		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
 		Point(erosion_size, erosion_size));
-	erode(srcGray, srcGray, element);
-	imshow("output", srcGray);
+	erode(img, img, element);
+	return img;
+}
 
-	//vector<vector<Vec2i>> lightRegions = detectLightRegions(srcGray);
+void findAngles(Mat img)
+{
+	vector<Vec2i> angles = {};
 
-	//imshow("output", src);
+	vector<vector<Point>> contours; // Vector for storing contour
+	vector<Vec4i> hierarchy;
+	findContours(srcGray, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); // Find the contours in the image
+
+	vector<double> largestArea = { 0, 0, 0, 0 };
+	vector<int> largestContourIndexes = { 0, 0, 0, 0 };
+	for (int i = 0; i < contours.size(); i++) // iterate through each contour. 
+	{
+		double a = contourArea(contours[i], false);  //  Find the area of contour
+		for (int j = 0; j < 4; j++)
+		{
+			if (a > largestArea[j]) {
+				largestArea.insert(largestArea.begin() + j, a);
+				//for inserting with shift of other elements, refactor later
+				for (int k = largestContourIndexes.size() - 1; k > j; k--)
+				{
+					largestContourIndexes[k] = largestContourIndexes[k - 1];
+				}
+				largestContourIndexes[j] = i; //Store the index of largest contour
+				break;
+			}
+		}
+	}
+
+	//x and y for each of 5 points (upper-left, upper-right, lower-right, lower-left, centroid) for each of 4 largest conrours
+	vector<vector<Vec2i>> countoursImportantPointsCoordinates = {}; //final size should be 4
+	Point2f generalUpperLeft = Point2f(src.cols, src.rows);
+	Point2f generalUpperRight = Point2f(0, src.rows);
+	Point2f generalLowerRight = Point2f(0, 0);
+	Point2f generalLowerLeft = Point2f(src.cols, 0);
+	for (int i = 0; i < largestContourIndexes.size(); i++)
+	{
+		int maxX = 0;
+		int maxY = 0;
+		int minX = src.cols;
+		int minY = src.rows;
+		
+		for (int j = 0; j < contours[largestContourIndexes[i]].size(); j++)
+		{
+			int x = contours[largestContourIndexes[i]][j].x;
+			int y = contours[largestContourIndexes[i]][j].y;
+			if (x > maxX)
+			{
+				maxX = x;
+			} 
+			if (x < minX)
+			{
+				minX = x;
+			}
+			if (y > maxY)
+			{
+				maxY = y;
+			}
+			if (y < minY)
+			{
+				minY = y;
+			}
+		}
+		cout << i << "(" << largestContourIndexes[i] << "): minX = " << minX << ", maxX = " << maxX << ", minY = " << minY << ", maxY = " << maxY << endl;
+		Vec2i upperLeft = Vec2i(minX, minY);
+		Vec2i upperRight = Vec2i(maxX, minY);
+		Vec2i lowerRight = Vec2i(maxX, maxY);
+		Vec2i lowerLeft = Vec2i(minX, maxY);
+		Vec2i centroid = Vec2i((minX + maxX)/2, (minY + maxY)/2);
+		countoursImportantPointsCoordinates.push_back({ upperLeft, upperRight, lowerRight, lowerLeft, centroid });
+
+		if (pow(upperLeft[0] - 0, 2) + pow(upperLeft[1] - 0, 2) < pow(generalUpperLeft.x - 0, 2) + pow(generalUpperLeft.y - 0, 2))
+		{
+			generalUpperLeft.x = upperLeft[0];
+			generalUpperLeft.y = upperLeft[1];
+		}
+		if (pow(upperRight[0] - src.cols - 1, 2) + pow(upperRight[1] - 0, 2) < pow(generalUpperRight.x - src.cols - 1, 2) + pow(generalUpperRight.y- 0, 2))
+		{
+			generalUpperRight.x = upperRight[0];
+			generalUpperRight.y = upperRight[1];
+		}
+		if (pow(lowerRight[0] - src.cols - 1, 2) + pow(lowerRight[1] - src.rows - 1, 2) < pow(generalLowerRight.x - src.cols - 1, 2) + pow(generalLowerRight.y - src.rows - 1, 2))
+		{
+			generalLowerRight.x = lowerRight[0];
+			generalLowerRight.y = lowerRight[1];
+		}
+		if (pow(lowerLeft[0] - 0, 2) + pow(lowerLeft[1] - src.rows - 1, 2) < pow(generalLowerLeft.x - 0, 2) + pow(generalLowerLeft.y - src.rows - 1, 2))
+		{
+			generalLowerLeft.x = lowerLeft[0];
+			generalLowerLeft.y = lowerLeft[1];
+		}
+
+		//for testing purposes
+		inputQuad[0] = Point2f(generalUpperLeft.x, generalUpperLeft.y);
+		inputQuad[1] = Point2f(generalUpperRight.x, generalUpperRight.y);
+		inputQuad[2] = Point2f(generalLowerRight.x, generalLowerRight.y);
+		inputQuad[3] = Point2f(generalUpperLeft.x, generalLowerLeft.y);
+		for (int i = 0; i < 4; i++)
+		{
+			circle(src, inputQuad[i], 3, Scalar(0, 0, 255), 1, 8, 0);
+		}
+		imshow("points", src);
+		cout << "Press any key" << endl;
+		waitKey(0);
+	}
+
+	//for now i'll just assign it but i'll have to rewrite it eventually
+	inputQuad[0] = Point2f(generalUpperLeft.x, generalUpperLeft.y);
+	inputQuad[1] = Point2f(generalUpperRight.x, generalUpperRight.y);
+	inputQuad[2] = Point2f(generalLowerRight.x, generalLowerRight.y);
+	inputQuad[3] = Point2f(generalUpperLeft.x, generalLowerLeft.y);
+
+}
+
+int main(int argc, char** argv)
+{
+	src = imread("white.png", CV_LOAD_IMAGE_UNCHANGED);
+	srcGray = imread("white.png", CV_LOAD_IMAGE_GRAYSCALE);
+	//vector<Vec2f> lines = calculateHoughLines(srcGray);
+	//drawLines(srcGray, lines);
+	//imshow("with lines", srcGray);
+	//imwrite("lines.png", srcGray);
+	
+	srcGray = threasholdImage(srcGray);
+	imshow("white regions", srcGray);
+	findAngles(srcGray);
+
+	outputQuad[0] = Point2f(0, 0);
+	outputQuad[1] = Point2f(src.cols - 1, 0);
+	outputQuad[2] = Point2f(src.cols - 1, src.rows - 1);
+	outputQuad[3] = Point2f(0, src.rows - 1);
+
+	Mat output = perspectiveTransform(src);
+
+	imshow("output", output);
+	imwrite("output.png", output);
 	waitKey();
 }
